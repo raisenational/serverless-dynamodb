a207e9a1ce63e46a9bd2dbfe86f65bfbf6690326
Adam Jones <domdomegg+git@gmail.com>
2024-02-13 22:32:01 +0000

    Add migration and seeding tests (#43)

--- src/serverlessMock.test.ts
+++ src/serverlessMock.test.ts
@@ -9,8 +9,48 @@ const serverlessMock = {
       dynamodb: {
         port: 8000,
         stages: ['test'],
+        seed: {
+          test: {
+            sources: [{
+              table: 'person-table',
+              sources: ['./test-resources/persons.sources.json'],
+            }, {
+              table: 'building-table',
+              rawsources: ['./test-resources/buildings.rawsources.json'],
+            }],
+          },
+        },
       },
     },
+    resources: {
+      Resources: {
+        personTable: {
+          Type: 'AWS::DynamoDB::Table',
+          Properties: {
+            TableName: 'person-table',
+            AttributeDefinitions: [{ AttributeName: 'id', AttributeType: 'S' }],
+            KeySchema: [{ AttributeName: 'id', KeyType: 'HASH' }],
+            ProvisionedThroughput: {
+              ReadCapacityUnits: 1,
+              WriteCapacityUnits: 1,
+            },
+          },
+        },
+        buildingTable: {
+          Type: 'AWS::DynamoDB::Table',
+          Properties: {
+            TableName: 'building-table',
+            AttributeDefinitions: [{ AttributeName: 'id', AttributeType: 'S' }],
+            KeySchema: [{ AttributeName: 'id', KeyType: 'HASH' }],
+            ProvisionedThroughput: {
+              ReadCapacityUnits: 1,
+              WriteCapacityUnits: 1,
+            },
+          },
+        },
+      },
+    },
+
   },
   cli: {
     log: () => { },
