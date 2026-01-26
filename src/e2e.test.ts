bc0f5abd3946ae21e8c5fc0d0c032b36d072ddd8
Stefan Person <stefan.person@neteleven.de>
2026-01-26 11:34:48 +0100

    chore: Upgrade dependencies
    While doing so, fixed linter issues. Some are not fixable as long as serverless v2 should be supported

--- src/e2e.test.ts
+++ src/e2e.test.ts
@@ -1,92 +1,92 @@
 import {
-  test, expect, beforeAll, afterAll,
+	test, expect, beforeAll, afterAll,
 } from 'vitest';
 import {
-  CreateTableCommand, DynamoDBClient, ListTablesCommand, PutItemCommand, ScanCommand,
+	CreateTableCommand, DynamoDBClient, ListTablesCommand, PutItemCommand, ScanCommand,
 } from '@aws-sdk/client-dynamodb';
 import ServerlessDynamoDBPlugin from './index';
 import serverlessMock from './serverlessMock.test';
 
 const client = new DynamoDBClient({
-  region: 'localhost',
-  endpoint: 'http://0.0.0.0:8000',
-  credentials: {
-    accessKeyId: 'MockAccessKeyId',
-    secretAccessKey: 'MockSecretAccessKey',
-  },
+	region: 'localhost',
+	endpoint: 'http://0.0.0.0:8000',
+	credentials: {
+		accessKeyId: 'MockAccessKeyId',
+		secretAccessKey: 'MockSecretAccessKey',
+	},
 });
 
 const testTableName = 'serverless-dynamodb-test-things';
 const testItem = {
-  id: { S: 'thing_123' },
-  name: { S: 'example string' },
+	id: {S: 'thing_123'},
+	name: {S: 'example string'},
 };
 
 let plugin: ServerlessDynamoDBPlugin;
 
 beforeAll(async () => {
-  plugin = new ServerlessDynamoDBPlugin(serverlessMock, { stage: 'test', region: 'test' });
-  await plugin.installHandler();
-  await plugin.startHandler();
+	plugin = new ServerlessDynamoDBPlugin(serverlessMock, {stage: 'test', region: 'test'});
+	await plugin.installHandler();
+	await plugin.startHandler();
 }, 30_000);
 
 test('create and list tables, add and scan items', async () => {
-  await client.send(new CreateTableCommand({
-    AttributeDefinitions: [{
-      AttributeName: 'id',
-      AttributeType: 'S',
-    }],
-    KeySchema: [{
-      AttributeName: 'id',
-      KeyType: 'HASH',
-    }],
-    TableName: testTableName,
-    ProvisionedThroughput: {
-      ReadCapacityUnits: 5,
-      WriteCapacityUnits: 5,
-    },
-  }));
+	await client.send(new CreateTableCommand({
+		AttributeDefinitions: [{
+			AttributeName: 'id',
+			AttributeType: 'S',
+		}],
+		KeySchema: [{
+			AttributeName: 'id',
+			KeyType: 'HASH',
+		}],
+		TableName: testTableName,
+		ProvisionedThroughput: {
+			ReadCapacityUnits: 5,
+			WriteCapacityUnits: 5,
+		},
+	}));
 
-  const tables = await client.send(new ListTablesCommand({}));
-  expect(tables.TableNames).toEqual([testTableName]);
+	const tables = await client.send(new ListTablesCommand({}));
+	expect(tables.TableNames).toEqual([testTableName]);
 
-  await client.send(new PutItemCommand({
-    TableName: testTableName,
-    Item: testItem,
-  }));
+	await client.send(new PutItemCommand({
+		TableName: testTableName,
+		Item: testItem,
+	}));
 
-  const items = await client.send(new ScanCommand({ TableName: testTableName }));
-  expect(items?.Items).toHaveLength(1);
-  expect(items.Items![0]).toEqual(testItem);
+	const items = await client.send(new ScanCommand({TableName: testTableName}));
+	expect(items?.Items).toHaveLength(1);
+	expect(items.Items![0]).toEqual(testItem);
 });
 
 test('can migrate and seed tables', async () => {
-  const expectedTables = ['person-table', 'building-table'];
+	const expectedTables = ['person-table', 'building-table'];
 
-  // The tables do not exist at the beginning
-  const tablesBeforeMigration = await client.send(new ListTablesCommand({}));
-  expect(expectedTables.some((t) => tablesBeforeMigration.TableNames?.includes(t))).toBeFalsy();
+	// The tables do not exist at the beginning
+	const tablesBeforeMigration = await client.send(new ListTablesCommand({}));
+	expect(expectedTables.some((t) => tablesBeforeMigration.TableNames?.includes(t))).toBeFalsy();
 
-  await plugin.migrateHandler();
+	await plugin.migrateHandler();
 
-  // After migration, the tables exist and are empty
-  const tablesAfterMigration = await client.send(new ListTablesCommand({}));
-  expect(expectedTables.every((t) => tablesAfterMigration.TableNames?.includes(t))).toBeTruthy();
-  await Promise.all(expectedTables.map(async (tableName) => {
-    const items = await client.send(new ScanCommand({ TableName: tableName }));
-    expect(items.Items?.length, tableName).toBe(0);
-  }));
+	// After migration, the tables exist and are empty
+	const tablesAfterMigration = await client.send(new ListTablesCommand({}));
+	expect(expectedTables.every((t) => tablesAfterMigration.TableNames?.includes(t))).toBeTruthy();
+	await Promise.all(expectedTables.map(async (tableName) => {
+		const items = await client.send(new ScanCommand({TableName: tableName}));
+		expect(items.Items?.length, tableName).toBe(0);
+	}));
 
-  await plugin.seedHandler();
+	await plugin.seedHandler();
 
-  // After seeding, the tables are not empty
-  // await new Promise((resolve) => setTimeout(resolve, 1000));
-  await Promise.all(expectedTables.map(async (tableName) => {
-    const items = await client.send(new ScanCommand({ TableName: tableName }));
-    expect(items.Items?.length, tableName).not.toBe(0);
-  }));
+	// After seeding, the tables are not empty
+	// await new Promise((resolve) => setTimeout(resolve, 1000));
+	await Promise.all(expectedTables.map(async (tableName) => {
+		const items = await client.send(new ScanCommand({TableName: tableName}));
+		expect(items.Items?.length, tableName).not.toBe(0);
+	}));
 });
 
 afterAll(async () => {
-  await plugin.endHandler();
+	await plugin.endHandler();
 });
--- src/seeder.ts
+++ src/seeder.ts
@@ -1,15 +1,15 @@
 import path from 'node:path';
 import fs from 'node:fs';
-import { BatchWriteItemCommandInput, BatchWriteItemCommandOutput } from '@aws-sdk/client-dynamodb';
-import { BatchWriteCommandInput, BatchWriteCommandOutput } from '@aws-sdk/lib-dynamodb';
+import {type BatchWriteItemCommandInput, type BatchWriteItemCommandOutput} from '@aws-sdk/client-dynamodb';
+import {type BatchWriteCommandInput, type BatchWriteCommandOutput} from '@aws-sdk/lib-dynamodb';
 
 // DynamoDB has a 25 item limit in batch requests
 // https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_BatchWriteItem.html
 const MAX_MIGRATION_CHUNK = 25;
 
 type DynamoDBWriteFunction = {
-  (params: BatchWriteCommandInput): Promise<BatchWriteCommandOutput>;
-  (params: BatchWriteItemCommandInput): Promise<BatchWriteItemCommandOutput>;
+	(params: BatchWriteCommandInput): Promise<BatchWriteCommandOutput>;
+	(params: BatchWriteItemCommandInput): Promise<BatchWriteItemCommandOutput>;
 };
 
 /**
@@ -17,39 +17,41 @@ type DynamoDBWriteFunction = {
  * items that may be written in a batch operation.
  * @param {function} dynamodbWriteFunction The DynamoDB DocumentClient.batchWrite or DynamoDB.batchWriteItem function
  */
-function writeSeedBatch(dynamodbWriteFunction: DynamoDBWriteFunction, tableName: string, seedValues: object[]) {
-  const params = {
-    RequestItems: {
-      [tableName]: seedValues.map((seed) => ({
-        PutRequest: {
-          Item: seed,
-        },
-      })),
-    },
-  };
-  return new Promise<void>((resolve, reject) => {
-    // interval lets us know how much time we have burnt so far. This lets us have a backoff mechanism to try
-    // again a few times in case the Database resources are in the middle of provisioning.
-    function execute(interval: number) {
-      setTimeout(async () => {
-        try {
-          await dynamodbWriteFunction(params);
-          resolve();
-        } catch (err) {
-          if (err) {
-            if (err instanceof Error && 'code' in err && err.code === 'ResourceNotFoundException' && interval <= 5000) {
-              execute(interval + 1000);
-            } else if (err instanceof TypeError && err.message === "Cannot read properties of undefined (reading '0')") {
-              reject(new Error(`Failed to seed items for the ${tableName} table because of an AWS library error. This usually means your \`rawsources\` seed files are invalid.`, { cause: err }));
-            } else {
-              reject(err);
-            }
-          }
-        }
-      }, interval);
-    }
-    execute(0);
-  });
+async function writeSeedBatch(dynamodbWriteFunction: DynamoDBWriteFunction, tableName: string, seedValues: object[]) {
+	const params = {
+		RequestItems: {
+			[tableName]: seedValues.map((seed) => ({
+				PutRequest: {
+					Item: seed,
+				},
+			})),
+		},
+	};
+	return new Promise<void>((resolve, reject) => {
+		// interval lets us know how much time we have burnt so far. This lets us have a backoff mechanism to try
+		// again a few times in case the Database resources are in the middle of provisioning.
+		function execute(interval: number) {
+			setTimeout(async () => {
+				try {
+					await dynamodbWriteFunction(params);
+					resolve();
+				} catch (err) {
+					if (err) {
+						if (err instanceof Error && 'code' in err && err.code === 'ResourceNotFoundException' && interval <= 5000) {
+							execute(interval + 1000);
+						} else if (err instanceof TypeError && err.message === 'Cannot read properties of undefined (reading \'0\')') {
+							reject(new Error(`Failed to seed items for the ${tableName} table because of an AWS library error. This usually means your \`rawsources\` seed files are invalid.`, {cause: err}));
+						} else {
+							// eslint-disable-next-line @typescript-eslint/prefer-promise-reject-errors
+							reject(err);
+						}
+					}
+				}
+			}, interval);
+		}
+
+		execute(0);
+	});
 }
 
 /**
@@ -57,26 +59,28 @@ function writeSeedBatch(dynamodbWriteFunction: DynamoDBWriteFunction, tableName:
  * @param dynamodbWriteFunction The DynamoDB DocumentClient.batchWrite or DynamoDB.batchWriteItem function
  */
 export async function writeSeeds(dynamodbWriteFunction: DynamoDBWriteFunction, tableName: string, seedValues: object[]) {
-  if (!dynamodbWriteFunction) {
-    throw new Error('dynamodbWriteFunction argument must be provided');
-  }
-  if (!tableName) {
-    throw new Error('table name argument must be provided');
-  }
-  if (!seedValues) {
-    throw new Error('seeds argument must be provided');
-  }
+	if (!dynamodbWriteFunction) {
+		throw new Error('dynamodbWriteFunction argument must be provided');
+	}
 
-  const seedChunks = chunk(seedValues, MAX_MIGRATION_CHUNK);
-  await Promise.all(seedChunks.map((chunk) => writeSeedBatch(dynamodbWriteFunction, tableName, chunk)));
+	if (!tableName) {
+		throw new Error('table name argument must be provided');
+	}
+
+	if (!seedValues) {
+		throw new Error('seeds argument must be provided');
+	}
+
+	const seedChunks = chunk(seedValues, MAX_MIGRATION_CHUNK);
+	await Promise.all(seedChunks.map(async (chunk) => writeSeedBatch(dynamodbWriteFunction, tableName, chunk)));
 }
 
 const chunk = <T>(input: T[], size: number): T[][] => {
-  return input.reduce<T[][]>((arr, item, idx) => {
-    return idx % size === 0
-      ? [...arr, [item]]
-      : [...arr.slice(0, -1), [...(arr.slice(-1)[0] ?? []), item]];
-  }, []);
+	return input.reduce<T[][]>((arr, item, idx) => {
+		return idx % size === 0
+			? [...arr, [item]]
+			: [...arr.slice(0, -1), [...(arr.slice(-1)[0] ?? []), item]];
+	}, []);
 };
 
 /**
@@ -88,28 +92,31 @@ const chunk = <T>(input: T[], size: number): T[][] => {
  * @returns {object[]} json
  */
 function getSeedsAtLocation(location: string) {
-  // load the file
-  const rawFile = fs.readFileSync(location, { encoding: 'utf-8' });
+	// load the file
+	const rawFile = fs.readFileSync(location, {encoding: 'utf-8'});
 
-  // parse as JSON
-  const parsed = safeParse(rawFile);
-  if (parsed instanceof Error) {
-    throw new Error(`Failed to parse JSON when reading seed file '${location}'`, parsed);
-  }
+	// parse as JSON
+	const parsed = safeParse(rawFile);
+	if (parsed instanceof Error) {
+		throw new Error(`Failed to parse JSON when reading seed file '${location}'`, parsed);
+	}
 
-  // ensure the output is an array
-  const array: object[] = Array.isArray(parsed) ? parsed : [parsed];
+	// ensure the output is an array
+	const array: object[] = Array.isArray(parsed) ? parsed : [parsed];
 
-  return array;
+	return array;
 }
 
 function safeParse(json: string): object | Error {
-  try {
-    return JSON.parse(json);
-  } catch (err) {
-    if (err instanceof Error) return err;
-    return new Error(String(err));
-  }
+	try {
+		return JSON.parse(json);
+	} catch (err) {
+		if (err instanceof Error) {
+			return err;
+		}
+
+		return new Error(String(err));
+	}
 }
 
 /**
@@ -118,11 +125,12 @@ function safeParse(json: string): object | Error {
  * @returns {object[]} The items to seed
  */
 export function locateSeeds(sources: string[], cwd: string = process.cwd()) {
-  const locations = sources.map((source) => path.join(cwd, source));
-  return locations.map((location) => {
-    if (!fs.existsSync(location)) {
-      throw new Error(`source file ${location} does not exist`);
-    }
-    return getSeedsAtLocation(location);
-  }).flat(1);
+	const locations = sources.map((source) => path.join(cwd, source));
+	return locations.map((location) => {
+		if (!fs.existsSync(location)) {
+			throw new Error(`source file ${location} does not exist`);
+		}
+
+		return getSeedsAtLocation(location);
+	}).flat(1);
 }
